17085918bb8c8554305b1b321dbfa76d650dc513
Oleksii <56698837+oleksii-suvorov@users.noreply.github.com>
2020-05-17 23:56:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 app = express();
 
 app.get("/", function(req,res){
-  res.sendFile("index.html");
+  res.sendFile("/index.html");
 })
 
 app.listen(process.env.PORT, process.env.IP);
