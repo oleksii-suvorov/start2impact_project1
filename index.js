2505f9a6bc3c3501236deef90378d543ab9e814a
Oleksii <56698837+oleksii-suvorov@users.noreply.github.com>
2020-05-17 23:43:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+app = express();
+
+app.use(express.static(__dirname + "/"));
+
+app.get("/", function(req,res){
+  res.sendFile("index.html");
+})
+
+app.listen(process.env.PORT, process.env.IP);
